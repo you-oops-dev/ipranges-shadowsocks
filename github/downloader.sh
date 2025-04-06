771eb448adf19d92b4883e041cd2b641735e782d
you-oops-dev[silent] <you-oops-dev[silent]@users.noreply.github.com>
2025-04-06 05:50:39 +0300

    Add domain in Github acl

--- github/downloader.sh
+++ github/downloader.sh
@@ -4,6 +4,25 @@
 set -euo pipefail
 set -x
 
+# Add domain in ACL file
+add_domain() {
+curl --max-time 30 --retry-delay 3 --retry 10 -4s -# https://raw.githubusercontent.com/$NAME_ACCOUNT_GITHUB/ipranges/main/github/domains.txt > /tmp/"$1"_domain.txt
+dos2unix /tmp/"$1"_domain.txt
+sort /tmp/"$1"_domain.txt | uniq | sponge /tmp/"$1"_domain.txt
+# Prepare domain
+# Add domain (Fixing)
+echo "githubusercontent.com
+core.windows.net" >> /tmp/"$1"_domain.txt
+# Delete subdomain in file
+cat /tmp/"$1"_domain.txt | grep -vEe '(.githubusercontent.com|.github.com|.core.windows.net)$' > /tmp/"$1"_domain_prepare.txt
+sort -h /tmp/"$1"_domain_prepare.txt | uniq | sponge /tmp/"$1"_domain_prepare.txt
+# Replace . on \.
+sed -i 's/\./\\./g' /tmp/"$1"_domain_prepare.txt
+# ipv4
+for domain in $(cat /tmp/${1}_domain_prepare.txt); do echo \(\?\:\^\|\\\.\)${domain}$ >> ${1}/ipv4.acl; done
+# ipv6
+#for domain in $(cat /tmp/${1}_domain_prepare.txt); do echo \(\?\:\^\|\\\.\)${domain}$ >> ${1}/ipv6.acl; done
+}
 
 # get CIDR from list
 get_prefix() {
@@ -24,6 +43,8 @@ echo -e "[bypass_all]\n[proxy_list]" | tee github/ipv4.acl
 
 # Create/Prepare ACL List for Shadowsocks IPv6
 #echo -e "[bypass_all]\n[proxy_list]" | tee github/ipv6.acl
+
+add_domain 'github' || echo 'failed'
 
 # sort & uniq
 sort -h /tmp/github-ipv4.txt | uniq >> github/ipv4.acl
