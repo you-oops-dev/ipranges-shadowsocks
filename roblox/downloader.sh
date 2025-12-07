c2dd70ec02906e1bb3e447982dc85cbc890efa43
you-oops-dev[silent] <you-oops-dev[silent]@users.noreply.github.com>
2025-12-07 09:08:13 +0300

    Edit script add domains

--- roblox/downloader.sh
+++ roblox/downloader.sh
@@ -4,11 +4,29 @@
 set -euo pipefail
 set -x
 
+# Add domain in ACL file
+add_domain() {
+curl --max-time 30 --retry-delay 3 --retry 10 -4s -# https://raw.githubusercontent.com/$NAME_ACCOUNT_GITHUB/ipranges/refs/heads/main/roblox/domain.txt > /tmp/"$1"_domain.txt
+dos2unix /tmp/"$1"_domain.txt
+sort /tmp/"$1"_domain.txt | uniq | sponge /tmp/"$1"_domain.txt
+# Prepare domain
+# Delete subdomain in file
+#cat /tmp/"$1"_domain.txt | grep -vEe '(.openai.com|.openai.org|.openai.com.cdn.cloudflare.net|.oaistatic.com)$' > /tmp/"$1"_domain_prepare.txt
+#sort -h /tmp/"$1"_domain_prepare.txt | uniq | sponge /tmp/"$1"_domain_prepare.txt
+# Replace . on \.
+#sed -i 's/\./\\./g' /tmp/"$1"_domain_prepare.txt
+sed -i 's/\./\\./g' /tmp/"$1"_domain.txt
+mv -f /tmp/"$1"_domain.txt /tmp/"$1"_domain_prepare.txt
+# ipv4
+for domain in $(cat /tmp/${1}_domain_prepare.txt | sed '/.tk/d'); do echo \(\?\:\^\|\\\.\)${domain}$ >> ${1}/ipv4.acl; done
+# ipv6
+#for domain in $(cat /tmp/${1}_domain_prepare.txt); do echo \(\?\:\^\|\\\.\)${domain}$ >> ${1}/ipv6.acl; done
+}
 
 # get CIDR from list
 get_prefix() {
 curl --max-time 30 --retry-delay 3 --retry 10 -4s -# https://raw.githubusercontent.com/$NAME_ACCOUNT_GITHUB/ipranges/main/"$1"/ipv4_merged.txt > /tmp/"$1".txt
-curl --max-time 30 --retry-delay 3 --retry 10 -4s -# https://raw.githubusercontent.com/$NAME_ACCOUNT_GITHUB/ipranges/main/"$1"/ipv6_merged.txt >> /tmp/"$1".txt
+#curl --max-time 30 --retry-delay 3 --retry 10 -4s -# https://raw.githubusercontent.com/$NAME_ACCOUNT_GITHUB/ipranges/main/"$1"/ipv6_merged.txt >> /tmp/"$1".txt
 }
 
 get_prefix 'roblox' || echo 'failed'
@@ -24,6 +42,8 @@ echo -e "[bypass_all]\n[proxy_list]" | tee roblox/ipv4.acl
 
 # Create/Prepare ACL List for Shadowsocks IPv6
 #echo -e "[bypass_all]\n[proxy_list]" | tee roblox/ipv6.acl
+
+add_domain 'roblox' || echo 'failed'
 
 # sort & uniq
 sort -h /tmp/roblox-ipv4.txt | uniq >> roblox/ipv4.acl
